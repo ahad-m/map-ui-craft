10a23825dc9784eb4cf0c85e63aad30ae216a3c6
AHAD <ahad.malotaibii@gmail.com>
2025-12-08 15:12:00 +0300

    Update gunicorn_config.py

--- Backend/gunicorn_config.py
+++ Backend/gunicorn_config.py
@@ -3,7 +3,6 @@ bind = "0.0.0.0:8000"
 workers = 2  # عدد العمال (processors)
 worker_class = "uvicorn.workers.UvicornWorker"
 
-# هذا هو أهم سطر لمشروعك
-# نعطيه 3 دقائق (180 ثانية) كوقت مستقطع (timeout)
+
 # هذا ضروري ليتحمل تحميل موديل BGE-M3 عند بدء التشغيل
 timeout = 180
